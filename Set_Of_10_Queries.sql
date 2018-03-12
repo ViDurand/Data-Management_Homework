da35588096d49bfaa50b6463a5cce328773ab5dd
ViDurand <34512615+ViDurand@users.noreply.github.com>
2018-03-12 15:20:33 +0100

    Add files via upload

---
+++ Set_Of_10_Queries.sql
@@ -0,0 +1,57 @@
+
+-- Query 1:
+-- 		What are the names of all the languages in the database (sorted alphabetically)?
+
+
+
+-- Query 2:
+-- 		Return the full names (first and last) of actors with “SON” in their last name, ordered by their first name.
+
+
+
+
+-- Query 3:
+-- 		Find all the addresses where the second address is not empty (i.e., contains some text), and return 
+-- 		these second addresses sorted.
+
+
+
+-- Query 4:
+-- 		Return the first and last names of actors who played in a film involving a “Crocodile” and a “Shark”,
+-- 		along with the release year of the movie, sorted by the actors’ last names.
+
+
+
+
+-- Query 5:
+-- 		How many films involve a “Crocodile” and a “Shark”?
+
+
+
+-- Query 6:
+-- 		What is the average running time of films by category?
+
+
+
+-- Query 7:
+-- 		Which actor has appeared in the most films?
+
+
+
+-- Query 8:
+-- 		When is ‘Academy Dinosaur’ due?
+
+
+
+
+-- Query 9:
+-- 		Find all the film categories in which there are between 55 and 65 films. Return the names of these
+-- 		categories and the number of films per category, sorted by the number of films.
+
+
+
+
+-- Query 10:
+-- 		In how many film categories is the average difference between the film replacement cost and the
+-- 		rental rate larger than 17?
+
